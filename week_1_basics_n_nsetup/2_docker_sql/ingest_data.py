59dcdb2ffa783b9e78c4e543e18d5d9b8f676a41
Naomi Wu <37064515+naomi-wu-welly@users.noreply.github.com>
2024-01-18 23:05:17 +1300

    Putting the ingestion script into Docker - PARQUET

--- week_1_basics_n_nsetup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_nsetup/2_docker_sql/ingest_data.py
@@ -3,15 +3,52 @@
 import argparse
 from time import time
 import pandas as pd
+import argparse
+import requests
 from sqlalchemy import create_engine
 
-df = pd.read_parquet("yellow_tripdata_2021-01.parquet")
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
 
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    parquet_name = 'output.parquet'
 
-engine.connect()
+    # download the PARQUET file
+    response = requests.get(url)
+    with open(parquet_name, 'wb') as file:
+        file.write(response.content)
 
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    df = pd.read_parquet(parquet_name)
+    t_start = time()
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+    t_end = time()
+    print('Took %.3f second' %(t_end - t_start))
 
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest Parquet data to Postgres')
+    # user
+    # password
+    # host
+    # port
+    # database name
+    # table name
+    # url of the csv
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the csv file')
+
+    args = parser.parse_args()
+
+    main(args)
